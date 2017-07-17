dd3f42c72f7189a2350080e3ed9f3a29faed7623
Ali Reza Barkhordari <abarkhordari@ce.sharif.edu>
2017-07-17 17:12:50 +0430

    After enabling Xposed

--- runtime/stack.h
+++ runtime/stack.h
@@ -335,6 +335,10 @@ class ShadowFrame {
     return method_;
   }
 
+  void SetMethod(ArtMethod* method) SHARED_REQUIRES(Locks::mutator_lock_) {
+    method_ = method;
+  }
+
   mirror::Object* GetThisObject() const SHARED_REQUIRES(Locks::mutator_lock_);
 
   mirror::Object* GetThisObject(uint16_t num_ins) const SHARED_REQUIRES(Locks::mutator_lock_);
@@ -605,6 +609,14 @@ class StackVisitor {
   ArtMethod* GetOuterMethod() const {
     return *GetCurrentQuickFrame();
   }
+
+  void SetMethod(ArtMethod* method) SHARED_REQUIRES(Locks::mutator_lock_) {
+	  if (cur_shadow_frame_ != nullptr) {
+	    cur_shadow_frame_->SetMethod(method);
+	  } else if (cur_quick_frame_ != nullptr) {
+	    *cur_quick_frame_ = method;
+	  }
+	}
 
   bool IsShadowFrame() const {
     return cur_shadow_frame_ != nullptr;
